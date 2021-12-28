84ee3fae5858d3caecfa97981e219075f6ff11c2
Jacquet BENJAMIN <jabenjam@e2r9p3.clusters.42paris.fr>
2021-12-28 14:42:43 +0100

    workflow

---
+++ philo/srcs/philo.c
@@ -0,0 +1,36 @@
+#include "../incs/philo.h"
+
+// Routine des philos :
+//  - Prendre une fourchette
+//  - Prendre l'autre fourchette
+//  - Manger
+//  - Lacher la premiere fourchette
+//  - Lacher la deuxieme fourchette
+//  - Dormir
+//  - Penser
+//  - Arreter
+
+// Lancement des threads
+
+// Creation et initialisation des mutex (fourchettes)
+
+// Creation et initialisation des threads (philosophes)
+
+// Gestion d'erreurs
+//  - Aucun philosophe
+//  - Valeur invalide
+//  - Mauvais nombre d'arguments
+//  - 
+
+// Parsing des arguments :
+//  - Nombre de philosophes
+//  - Temps pour mourir
+//  - Temps pour manger
+//  - Temps pour dormir
+//  - Nombre de repas max (OPTIONNEL)
+
+
+int main(int ac, char **av)
+{
+    
+}
